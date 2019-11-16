c3d954f18eaa82874a1e23d96d50f14cba4bfc27
HayleyKwok1 <wingk1226@gmail.com>
2019-11-16 16:33:36 +0000

    finished version of data

---
+++ data-analysis/getData.py
@@ -0,0 +1,24 @@
+import json
+
+with open('data.json') as json_file:
+    data_all = json.load(json_file)
+
+def getSingleElem(time1,elem1):
+    return data_all[time1][0][elem1]
+
+def getElemArray(elem2):
+    elemArray = []
+    for x in data_all:
+        elemArray.append(data_all[x][0][elem2]) 
+    return elemArray
+
+temp_array = getElemArray("temperature")
+hum_array = getElemArray("humidity")
+co2_array = getElemArray("co2")
+key_array = []
+for x in data_all:    
+    key_array.append(x)
+
+print(getSingleElem(key_array[0],"temperature"))
+
+
