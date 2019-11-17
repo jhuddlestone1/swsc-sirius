c9756d215a25db6b988396b44721e687d0fb4cf7
Yash Ghelani <yashghelani08@gmail.com>
2019-11-17 11:03:40 +0000

    motors moving with data from JSON

--- python/multimotor.py
+++ python/multimotor.py
@@ -3,6 +3,7 @@ import sys
 import time
 from time import sleep
 import json
+import math
 
 GPIO.setwarnings(False)
 
@@ -14,10 +15,14 @@ motor2 = [23,14]
 motor3 = [23,8]
 motor4 = [23,20]
 
-tempheight = 22.5
-humheight = 27.0
-co2height = 364.0
 timeheight = 1489968030000
+tempheight = 23.5
+humheight = 36.0
+co2height = 538.0
+
+tempAvg = 23.2
+humAvg = 36.0
+co2Avg = 538.0
 
 GPIO.setmode(GPIO.BCM)
 
@@ -30,28 +35,33 @@ for e in enabler:
 for i in inputs:
     GPIO.output(i,GPIO.LOW)
     
-p0=GPIO.PWM(enabler[0],1000)
-p1=GPIO.PWM(enabler[1],1000)
-p2=GPIO.PWM(enabler[2],1000)
-p3=GPIO.PWM(enabler[3],1000)
+p0=GPIO.PWM(enabler[0],500)
+p1=GPIO.PWM(enabler[1],500)
+p2=GPIO.PWM(enabler[2],500)
+p3=GPIO.PWM(enabler[3],500)
 
-p0.start(75)
-p1.start(75)
-p2.start(75)
-p3.start(75)
+p0.start(100)
+p1.start(100)
+p2.start(100)
+p3.start(100)
+
+def setup():
+    forward(motor2, 10)
+    forward(motor3, 10)
+    forward(motor4, 10)
 
 def forward(m, t):
     print("forward")
     GPIO.output(m[0],GPIO.HIGH)
     GPIO.output(m[1],GPIO.LOW)
-    time.sleep(t)
+    time.sleep(abs(t))
     stop(m)
 
 def backward(m, t):
     print("backward")
     GPIO.output(m[0],GPIO.LOW)
     GPIO.output(m[1],GPIO.HIGH)
-    time.sleep(t)
+    time.sleep(abs(t))
     stop(m)
     
 def stop(m):
@@ -101,28 +111,62 @@ def m4Action(m, a, t):
         print("please enter the defined data to continue.....")
 
 # read file
-with open('data.json', 'r') as myfile:
+with open('../data-analysis/data.json', 'r') as myfile:
     data = myfile.read()
 
 # parse file
 obj = json.loads(data)
 
-while(1):
-	
-    m = input("Choose a motor: ")
-    a = input("Choose an action: ")
-    v = input("Value: ")
+setup()
+
+for key in obj:
     
-    if m == "m1":
-        t = v - timeheight
-        m1Action(motor1, a, t)
-    elif m == "m2":
-        t = v - tempheight * 16
-        m2Action(motor2, a, t)
-    elif m == "m3":
-        t = v - humheight
-        m3Action(motor3, a, t)
-    elif m == "m4":
-        t = v - co2height * 0.05
+    print(obj[key])
+    
+    timeVal = int(key)
+    tempVal = int(obj[key]["temperature"])
+    humVal = int(obj[key]["humidity"])
+    co2Val = int(obj[key]["co2"])
+    
+    ############################
+    
+    t = (timeVal - timeheight) * 0.0002
+    print(t)
+    timeheight = timeVal
+    if t > 0:
+        m1Action(motor1, 'f', t)
+    else:
+        m1Action(motor1, 'b', t)
+    
+    ############################
+
+    t = (tempVal - tempheight) * 10
+    print(t)
+    tempheight = tempVal
+    if t > 0:
+        m2Action(motor2, 'f', t)
+    else:
+        m2Action(motor2, 'b', t)
+        
+    #############################
+        
+    t = (humVal - humheight) * 5
+    print(t)
+    humheight = humVal
+    if t > 0:
+        m3Action(motor3, 'f', t)
+    else:
+        m3Action(motor3, 'b', t)
+    
+    #############################
+    
+    t = (co2Val - co2height) / 75
+    print(t)
+    co2height = co2Val
+    if t > 0:
+        m4Action(motor4, 'f', t)
+    else:
+        m4Action(motor4, 'b', t)
+
 	
 GPIO.cleanup()
